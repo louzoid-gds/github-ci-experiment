a50dfb2f33210cec6cee6d3efbf0e351201db7e8
Louse Ryan <louise.ryan@digital.cabinet-office.gov.uk>
2017-09-12 10:16:31 +0100

    payload isnt a thing

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/event_handler', function (req, res) {
-  var payload = req.body.payload;
+  //var payload = req.body.payload;
 
   var h = req.get('X-GitHub-Event');
   if (!h) res.status(403).send("Doesn't look like the request is from Github :/");
   if (h === "pull_request") {
-    if (payload.action === "opened") {
-      var pr = payload.pull_request;
+    if (req.body.action === "opened") {
+      var pr = req.body.pull_request;
       res.status(200).send("Yah that worked! PR title: " + pr.title);
     }
   }
 
-  res.status(200).send("Not a PR so i didn't do anything. Header: " + h + ". Action: " + payload.action);
+  res.status(200).send("Not a PR so i didn't do anything. Header: " + h);
 
 });
 
