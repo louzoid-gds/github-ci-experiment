e73ad6c5011e52edc9516e01b3c1e3220040bd29
Louse Ryan <louise.ryan@digital.cabinet-office.gov.uk>
2017-09-12 10:41:48 +0100

    ok being a bit mad

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,19 @@ var router = express.Router();
 
 router.post('/event_handler', function (req, res) {
 
+  var payload = req.body.payload;
   res.set('Access-Control-Allow-Origin', "*");
   res.set('Access-Control-Allow-Methods', 'GET, POST');
   
   var h = req.get('X-GitHub-Event');
   if (!h) res.status(403).send("Doesn't look like the request is from Github :/");
   if (h == "pull_request") {
-    if (req.body.action == "opened") {
-      var pr = req.body.pull_request;
+    if (payload.action == "opened") {
+      var pr = payload.pull_request;
       res.status(200).send("Yah that worked! PR title: " + pr.title);
     }
     else {
-      console.log("action not opened:" + req.body.action);
+      console.log("action not opened:" + payload.action);
     }
   }
 
