5c24c6a0ea5cd556895c215e6dd849f7d006a260
Louse Ryan <louise.ryan@digital.cabinet-office.gov.uk>
2017-09-12 11:42:53 +0100

    updating a status

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var github = require('octonode');
 
 router.post('/event_handler', function (req, res) {
 
   var payload = JSON.parse(req.body.payload);
+
   res.set('Access-Control-Allow-Origin', "*");
   res.set('Access-Control-Allow-Methods', 'GET, POST');
   
@@ -12,7 +14,10 @@ router.post('/event_handler', function (req, res) {
   if (h == "pull_request") {
     if (payload.action == "opened") {
       var pr = payload.pull_request;
-      res.status(200).send("Yah that worked! PR title: " + pr.title);
+      //todo: clean this up!!
+      updateStatus(pr, "pending", function(err, cbresp) {
+        res.status(200).send("Yah that worked! PR title: " + pr.title);
+      });
     }
     else {
       console.log("action not opened:" + payload.action);
@@ -22,5 +27,18 @@ router.post('/event_handler', function (req, res) {
   res.status(200).send("Not a PR so i didn't do anything. Header: " + h);
 
 });
+
+function updateStatus(pr, status, cb) {
+  var tk = process.env.GH-ACCESS-TOKEN;
+  var client = github.client(tk);
+
+  var ghrepo = client.repo(pr.base.repo.full_name);
+  ghrepo.status(pr.head.sha, {
+    "state": status,
+    "target_url": "",
+    "description": "Build pending. yeah."
+  }, cb);
+
+}
 
 module.exports = router;
