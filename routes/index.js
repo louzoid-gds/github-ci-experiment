d5067b322d87966589b63f3aac780c8dacc752d1
Louse Ryan <louise.ryan@digital.cabinet-office.gov.uk>
2017-09-12 09:41:51 +0100

    Pr specific changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 router.post('/event_handler', function (req, res) {
   var payload = req.body.payload;
-  res.status(200).send("Yah that worked!");
+
+  var h = req.get('HTTP_X_GITHUB_EVENT');
+  if (!h) res.status(403).send("Doesn't look like the request is from Github :/");
+  if (h === "pull_request") {
+    if (payload.action === "opened") {
+      var pr = payload.pull_request;
+      res.status(200).send("Yah that worked! PR title: " + pr.title);
+    }
+  }
+
+  res.status(200).send("Not a PR so i didn't do anything");
+  
 //   var qs = req.query.urls; //['http://staging.main.su.baigentpreview.com/8.id', 'ppp', 'http://staging.main.su.baigentpreview.com/about/107.id', 'http://staging.main.su.baigentpreview.com/AboutScriptureUnion/Vacancies/106126.id'];
 //   var urls = qs.split(',');
 //   console.log(urls);
